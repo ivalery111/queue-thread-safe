2f3923fdbd177365ba6095540748465217602878
Valery Ivanov <ivalery111@gmail.com>
2020-11-25 08:25:47 +0300

    fix: utils: implicit declaration of _safe_free function

--- include/utils.h
+++ include/utils.h
@@ -3,13 +3,13 @@
 
 #include <stdlib.h>
 
-static inline void safe_free(void *ptr) { _safe_free((void **)&(ptr)); }
-
 static inline void _safe_free(void **ptr) {
   if (ptr != NULL && *ptr != NULL) {
     free(*ptr);
     *ptr = NULL;
   }
 }
+
+static inline void safe_free(void *ptr) { _safe_free((void **)&(ptr)); }
 
 #endif
